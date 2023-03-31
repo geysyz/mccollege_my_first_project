c47dc9def0ba3fadec5665cfafcddcd0ec651492
GEYSA_PINHO <geysapinho@gmail.com>
2023-03-31 13:00:45 -0500

    Saving initial project files

---
+++ src/main/java/com/mccollege/Geysa/GeysaApplication.java
@@ -0,0 +1,13 @@
+package com.mccollege.Geysa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeysaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeysaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mccollege/Geysa/GeysaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mccollege.Geysa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeysaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
