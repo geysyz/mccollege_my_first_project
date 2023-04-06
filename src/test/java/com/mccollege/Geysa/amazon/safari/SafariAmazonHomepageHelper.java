4f6e93a27a65ce2ac6303f093c2df0825146f39a
GEYSA_PINHO <geysapinho@gmail.com>
2023-04-05 21:44:37 -0500

    Created helper class for amazon homepage testing

---
+++ src/test/java/com/mccollege/Geysa/amazon/safari/SafariAmazonHomepageHelper.java
@@ -0,0 +1,16 @@
+package com.mccollege.Geysa.amazon.safari;
+
+public class SafariAmazonHomepageHelper {
+    public static String HOME_PAGE_URL = "https://www.amazon.com/";
+    public static String HOME_PAGE_ADRESS_BUTTON_ID = "glow-ingress-line2";
+    public static String HOME_PAGE_SEARCH_BAR_ID = "twotabsearchtextbox";
+    public static String HOME_PAGE_DEPARTMENT_BUTTON_ID = "searchDropdownBox";
+    public static String HOME_PAGE_SEARCH_BUTTON_ID = "nav-search-submit-button";
+    public static String HOME_PAGE_LANGUAGE_BUTTON_ID = "icp-nav-flyout";
+    public static String HOME_PAGE_SIGNIN_BUTTON_ID = "nav-link-accountList";
+    public static String HOME_PAGE_ORDERS_BUTTON_ID = "nav-orders";
+    public static String HOME_PAGE_CART_BUTTON_ID = "nav-cart";
+
+
+
+}
